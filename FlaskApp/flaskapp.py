a3f34469b2c10bcc9d11009cf7d2c711a0b89f78
aaronhannon <aaronchannon1@gmail.com>
2019-11-05 16:21:38 +0000

    Predictions working correctly

--- FlaskApp/flaskapp.py
+++ FlaskApp/flaskapp.py
@@ -9,6 +9,7 @@ import base64
 from keras.models import load_model
 import gzip
 import io
+import cv2
 from io import StringIO
 from io import BytesIO
 
@@ -23,34 +24,45 @@ def getImage():
     
     image_b64 = request.values['imageBase64']
     
-    print(image_b64)
-    
     image_data = re.sub('^data:image/.+;base64,', '', image_b64)
-
-    print(image_data)
 
     decode_Image = base64.b64decode(image_data)
 
-    decode_Image = ~np.array(list(decode_Image)).astype(np.uint8) / 255.0
-    decode_Image.resize(1,784)
-
-    # #print(test_img)
-
-    # img = Image.open(BytesIO(decode_Image))
+    img = Image.open(BytesIO(decode_Image))
+    img = img.save("img.png")
+    imge = cv2.imread("img.png")
+    gray = cv2.cvtColor(imge, cv2.COLOR_BGR2GRAY)
     
-    # #img.show()
+    grayArray = ~np.array(list(gray)).reshape(1, 784).astype(np.uint8) /255
 
-    # image_np = np.array(img)
 
-    # pixels = np.resize(image_np, (1, 784)).astype(np.uint8) / 255
+    print(grayArray)
+    counter = 0
+
+
+    #Print Contents
+    for i in grayArray[0]:
+        #print(i,end=" ")
+        if i == 1:
+           print(".",end="")
+        else:
+           print("0",end="")
+        counter +=1
+
+        if counter == 28:
+            print("\n")
+            counter = 0
+    
+    
 
     
-    model = load_model("model.h5")
 
-    labelPredict = model.predict(decode_Image)
+    labelPredict = prediction(grayArray)
 
+
+    print("PREDICTION: ")
     print(labelPredict)
-    print(labelPredict.argmax())
+    #print(labelPredict.argmax())
 
     with gzip.open('mnist_data/t10k-images-idx3-ubyte.gz', 'rb') as f:
         test_img = f.read()
@@ -60,84 +72,29 @@ def getImage():
 
 
 
-    test_img = ~np.array(list(test_img[16:])).reshape(10000, 784).astype(np.uint8) / 255.0
+    test_img = ~np.array(list(test_img[16:])).reshape(10000, 784).astype(np.uint8) /255
     
+    # for i in test_img[6:7][0]:
+    # #print(i,end=" ")
+    #     if i == 1:
+    #         print(".",end="")
+    #     else:
+    #         print("0",end="")
+    #     counter +=1
 
-    output = model.predict(test_img[5:6])
+    #     if counter == 28:
+    #         print("\n")
+    #         counter = 0
 
+    #print(test_img[5:6])
+
+    output = prediction(test_img[5:6])
+
+    print("TEST PREDICTION")
     print(output)
-    print(output.argmax())
-
-    # for row in pixels:
-    #     for elem in row:
-    #         if elem > 127:
-    #             print("0", end=' ')
-    #         else:
-    #             print(".",end=' ')
-    #     print()
-
-
-    #print(pixels)
-
-    # pred = prediction(pixels)
-
-    # print("Prediction: ")
-    # print(pred)
-
-    #test_img = ~np.array(list(decode_Image)).reshape(10000, 784).astype(np.uint8) / 255.0
-    
-    
-    
-    #data = request.get_data()
-
-    #data.show()
-    #dimensions = (4, 784)
-    #print(data)
-    #print(data[22:])
-
-    #print(type(data))
-    #print(data)
-    #print(data[38:])
-    #pixels = np.asarray(data[38:], dtype='uint8')
-
-    #decode_Image = base64.b64decode(data[38:])
-
-    #decode_Image.show()
-
-    #print(decode_Image)
-
-    #print(type(decode_Image))
-
-    #print(decode_Image)
-
-    # img = Image.open(BytesIO(decode_Image))
-    
-    # img.show()
-
-    # print(img)
-
-    # test_img = ~np.array(list(data[38:])).reshape(1, 784).astype(np.uint8) / 255.0
-
-    # print(test_img)
-
-    # image is (28, 28)
-    #img = img.resize(dimensions, Image.ANTIALIAS)
-    # # pixels.shape == (28, 28, 4)
-    # pixels = np.asarray(img, dtype='uint8')
-
-    # for i in pixels:
-    #     print(i)
-    # # force (28, 28)
-    #pixels = np.resize(pixels, (1, 784))
-
-    
-
-    #pred = prediction(pixels)
-
-    # print("Prediction: ")
-    # print(pred)
+    #print(output.argmax())
 
     return app.send_static_file('index.html')
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True, threaded = False)
--- FlaskApp/kerasModel.py
+++ FlaskApp/kerasModel.py
@@ -39,32 +39,38 @@ with gzip.open('mnist_data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
 #                 print(".",end=' ')
 #         print()
 
-#Start a neural network, building it by layers.
-model = kr.models.Sequential()
+def build():
+    #Start a neural network, building it by layers.
+    model = kr.models.Sequential()
 
-# Add a hidden layer with 1000 neurons and an input layer with 784.
-model.add(kr.layers.Dense(units=600, activation='linear', input_dim=784))
-model.add(kr.layers.Dense(units=400, activation='relu'))
-# Add a three neuron output layer.
-model.add(kr.layers.Dense(units=10, activation='softmax'))
+    # Add a hidden layer with 1000 neurons and an input layer with 784.
+    model.add(kr.layers.Dense(units=600, activation='linear', input_dim=784))
+    model.add(kr.layers.Dense(units=400, activation='relu'))
+    # Add a three neuron output layer.
+    model.add(kr.layers.Dense(units=10, activation='softmax'))
 
-# Build the graph.
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # Build the graph.
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-with gzip.open('mnist_data/train-images-idx3-ubyte.gz', 'rb') as f:
-    train_img = f.read()
+    with gzip.open('mnist_data/train-images-idx3-ubyte.gz', 'rb') as f:
+        train_img = f.read()
 
-with gzip.open('mnist_data/train-labels-idx1-ubyte.gz', 'rb') as f:
-    train_lbl = f.read()
-    
-train_img = ~np.array(list(train_img[16:])).reshape(60000, 28, 28).astype(np.uint8) / 255.0
-train_lbl =  np.array(list(train_lbl[ 8:])).astype(np.uint8)
+    with gzip.open('mnist_data/train-labels-idx1-ubyte.gz', 'rb') as f:
+        train_lbl = f.read()
+        
+    train_img = ~np.array(list(train_img[16:])).reshape(60000, 28, 28).astype(np.uint8) / 255.0
+    train_lbl =  np.array(list(train_lbl[ 8:])).astype(np.uint8)
 
-inputs = train_img.reshape(60000, 784)
+    inputs = train_img.reshape(60000, 784)
 
-encoder = pre.LabelBinarizer()
-encoder.fit(train_lbl)
-outputs = encoder.transform(train_lbl)
+    encoder = pre.LabelBinarizer()
+    encoder.fit(train_lbl)
+    outputs = encoder.transform(train_lbl)
+
+    model.fit(inputs, outputs, epochs=4, batch_size=100)
+    model.save("model.h5")
+
+    return model
 
 #print(type(encoder))
 
@@ -75,35 +81,16 @@ outputs = encoder.transform(train_lbl)
 
 def prediction(image):
     try:
-        print("LOADING.....")
+        #print("LOADING.....")
         model = load_model("model.h5")
     except:
         print("LOADING FAILED....")
-        model.fit(inputs, outputs, epochs=2, batch_size=100)
-        model.save("model.h5")
+        model = build()
 
-
-    with gzip.open('mnist_data/t10k-images-idx3-ubyte.gz', 'rb') as f:
-        test_img = f.read()
-
-    with gzip.open('mnist_data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
-        test_lbl = f.read()
-
-
-
-    test_img = ~np.array(list(test_img[16:])).reshape(10000, 784).astype(np.uint8) / 255.0
-    test_lbl =  np.array(list(test_lbl[ 8:])).astype(np.uint8)
-
-    output = model.predict(test_img)
-
-    #print(encoder.inverse_transform(output))
-    #print(jk)
-    ##print(test_lbl[0:5])
-    #print((encoder.inverse_transform(model.predict(test_img)) == test_lbl).sum())
 
     
     labelPredict = model.predict(image)
-    print(labelPredict)
+    
     
     return labelPredict.argmax()
 
